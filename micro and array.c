d88b17376e38b305cd0683a01196f4ee2885819d
nmdnaveenkumar <53473816+nmdnaveenkumar@users.noreply.github.com>
2019-09-03 16:15:24 +0530

    Create micro and array.c

---
+++ micro and array.c
@@ -0,0 +1,25 @@
+
+
+#include<stdio.h>
+int main()
+{
+    int t,n,i,min=10,k;
+    scanf("%d",&t);
+    while(t--)
+    {
+     min=10;
+     scanf("%d%d",&n,&k);
+     int a[n];
+     for(i=0;i<n;i++)
+     {
+      scanf("%d",&a[i]);
+      if(a[i]<min)
+       min=a[i];
+     }
+     //printf("%d ",min);
+     if(min>=k)
+      printf("0\n");
+     else
+      printf("%d\n",k-min);
+    }
+    return 0;
